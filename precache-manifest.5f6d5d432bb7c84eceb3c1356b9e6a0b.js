68902ea0e1409ddaca7848d1fe99f88bed57129e
Enes Tüfekçi <enesxtufekci@gmail.com>
2019-04-06 01:16:26 +0200

    Updates

---
+++ precache-manifest.5f6d5d432bb7c84eceb3c1356b9e6a0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "41c7a6de0a8a97768da4",
+    "url": "/static/js/main.6903494d.chunk.js"
+  },
+  {
+    "revision": "2ed50eef9ca87305346c",
+    "url": "/static/js/2.8b084143.chunk.js"
+  },
+  {
+    "revision": "41c7a6de0a8a97768da4",
+    "url": "/static/css/main.233d1a1f.chunk.css"
+  },
+  {
+    "revision": "ddd9fa4867318ecbaa88dfb53369b871",
+    "url": "/index.html"
+  }
+];
